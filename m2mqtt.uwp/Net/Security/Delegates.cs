c82ec48a59b502edb5ce8cf46ba783f9361992a9
Nukepayload2 <1939357182@qq.com>
2017-03-17 00:37:47 +0800

    UWP build error fixed.

---
+++ m2mqtt.uwp/Net/Security/Delegates.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace System.Net.Security
+{
+    public delegate bool RemoteCertificateValidationCallback(
+        object sender,
+        X509Certificate certificate,
+        X509Chain chain,
+        SslPolicyErrors sslPolicyErrors
+    );
+
+    public delegate X509Certificate LocalCertificateSelectionCallback(
+        object sender,
+        string targetHost,
+        X509CertificateCollection localCertificates,
+        X509Certificate remoteCertificate,
+        string[] acceptableIssuers
+    );
+}
